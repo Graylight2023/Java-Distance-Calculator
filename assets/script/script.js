2f5b5db643bb98ff0dbb25ebafdd070eb8fdd4d1
Graylight2023 <aljulius.tamayo123@gmail.com>
2023-03-30 17:35:11 +0800

    no condition

--- assets/script/script.js
+++ assets/script/script.js
@@ -32,33 +32,10 @@ function display() {
 
     let x = document.getElementById("inputKilometer").value;
 
-            if(x == "" || x == null || x == undefined || x == 0){
-
-
-                document.getElementById("milesDisplay").innerHTML = 
-                "No Input for miles conversion";
-                
-
-                document.getElementById("footDisplay").innerHTML = 
-                "No Input for feet conversion";
-
-
-                document.getElementById("inchDisplay").innerHTML = 
-                "No Input for Inches conversion";
-
-                document.getElementById("meterDisplay").innerHTML = 
-                "No Input for Inches conversion";
-
-
-            }else{
-
-                
                 let miles = 0.621371;
                 let ft = 3280;
                 let inch = 10.07880;
                 let meter = 1000;
-                
-
 
                 document.getElementById("milesDisplay").innerHTML = 
                 "The Miles is = " + kilotomiles(x, miles);
@@ -73,11 +50,5 @@ function display() {
                 document.getElementById("meterDisplay").innerHTML = 
                 "The Meter is = " + kilotometer(x, meter);
 
-
-
-
-
             }
 
-
-}
